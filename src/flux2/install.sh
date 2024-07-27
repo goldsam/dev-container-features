f905e5ef994fc2f8b47ed5815242c8b893468561
Sam Goldmann <sam.goldmann@gmail.com>
2024-07-27 03:44:16 +0000

    pass empty FLUX_VERSION for latest

--- src/flux2/install.sh
+++ src/flux2/install.sh
@@ -2,20 +2,21 @@
 
 set -e
 
-FLUX_VERSION=${VERSION:-"latest"}
-
 export DEBIAN_FRONTEND=noninteractive
 
 echo "Activating feature 'flux2'"
 
-if [ "${FLUX_VERSION}" = "none" ] || type flux > /dev/null 2>&1; then
+if [ "${VERSION}" = "none" ] || type flux > /dev/null 2>&1; then
     echo "FLux already installed. Skipping..."
 else
+  if [ "${VERSION}" = "latest" ] ; then
+    VERSION=
+  else
 
   echo "Installing flux...'"
 
   # The official install script will honor FLUX_VERSION
-  curl -s https://fluxcd.io/install.sh | FLUX_VERSION=$FLUX_VERSION bash
+  curl -s https://fluxcd.io/install.sh | FLUX_VERSION=$VERSION bash
 fi
 
 set +e
