28eb1aed82f9537a956de43ce6e5f13ff012afd3
Sam Goldmann <sam.goldmann@gmail.com>
2024-07-27 03:53:00 +0000

    download instal script instead of piping

--- src/flux2/install.sh
+++ src/flux2/install.sh
@@ -10,13 +10,22 @@ if [ "${VERSION}" = "none" ] || type flux > /dev/null 2>&1; then
     echo "FLux already installed. Skipping..."
 else
   if [ "${VERSION}" = "latest" ] ; then
-    VERSION=
+    VERSION=''
   else
 
   echo "Installing flux...'"
 
   # The official install script will honor FLUX_VERSION
   curl -s https://fluxcd.io/install.sh | FLUX_VERSION=$VERSION bash
+
+  # Download the install script
+  curl -s -o install.sh https://fluxcd.io/install.sh
+
+  FLUX_VERSION=$VERSION bash install.sh
+
+  # Clean up
+  rm -f install.sh
+
 fi
 
 set +e
