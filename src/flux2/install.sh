7c724d96a11beadc24c3fa4d550b8ddb77887123
Sam Goldmann <sam.goldmann@gmail.com>
2024-07-27 03:58:56 +0000

    ensure FLUX_VERSION is not set for latest when invoking install.sh

--- src/flux2/install.sh
+++ src/flux2/install.sh
@@ -2,30 +2,24 @@
 
 set -e
 
+FLUX_VERSION=${VERSION:-"latest"}
+
 export DEBIAN_FRONTEND=noninteractive
 
 echo "Activating feature 'flux2'"
 
-if [ "${VERSION}" = "none" ] || type flux > /dev/null 2>&1; then
+if [ "${FLUX_VERSION}" = "none" ] || type flux > /dev/null 2>&1; then
     echo "FLux already installed. Skipping..."
 else
-  if [ "${VERSION}" = "latest" ] ; then
-    VERSION=''
-  else
 
   echo "Installing flux...'"
 
-  # The official install script will honor FLUX_VERSION
-  curl -s https://fluxcd.io/install.sh | FLUX_VERSION=$VERSION bash
-
-  # Download the install script
-  curl -s -o install.sh https://fluxcd.io/install.sh
-
-  FLUX_VERSION=$VERSION bash install.sh
-
-  # Clean up
-  rm -f install.sh
-
+  if [ "${FLUX_VERSION}" = "latest" ] ; then
+    curl -s https://fluxcd.io/install.sh | bash
+  else
+    curl -s https://fluxcd.io/install.sh | FLUX_VERSION=$FLUX_VERSION bash
+  fi
+  
 fi
 
 set +e
