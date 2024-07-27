481645a9976ae30c03f8e3b070789f770abd25bf
Sam Goldmann <sam.goldmann@gmail.com>
2024-07-27 04:02:01 +0000

    fixed bug in test

--- test/flux2/explicit_version.sh
+++ test/flux2/explicit_version.sh
@@ -10,7 +10,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "execute command" bash -c "flux -v | grep 'flux versionv 2.1.2'"
+check "execute command" bash -c "flux -v | grep 'flux version 2.1.2'"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
